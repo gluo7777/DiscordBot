6dcda645e4fed80b9c5c8070819c0445a6c57d41
William <gluo7777@gmail.com>
2021-10-01 13:15:20 -0500

    changed backend from s3 to tfe

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3"
+    }
+  }
+  backend "remote" {
+    organization = "williamluo7777"
+    workspaces {
+      name = "discord-bot"
+    }
+  }
+}
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "role_arn" {}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region  = "us-east-1"
+  profile = var.profile
+  assume_role {
+    role_arn = var.role_arn
+  }
+}
